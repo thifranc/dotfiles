687c73aa9575284e4f573609cfeeade5f454719f
Thibault François <thibault@octopuce.fr>
2019-06-19 18:31:28 +0200

    octo not to be versionned

--- bashUtils/octopuce.sh
+++ bashUtils/octopuce.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-
-phy() {
-    ssh root@$1 "awk '/Container/{print \$NF}' /etc/motd"
-}
-
-bck() {
-    ssh root@$1.octopuce.fr "grep 'server=' /etc/octobackup.conf|cut -d '=' -f 2|tr -d '\"'"
-}
+/home/thibault/octopuce/git/adminsys/scripts/bashUtils/octopuce.sh
